573017a324d940e36fa470a2ec6c681e97da1070
Nikolay Filatov <colyan.filatoff@yandex.ru>
2024-07-30 19:28:16 +0300

    Добавлены вьюсеты и сериализаторы Review, Comment

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,1 +1,61 @@
-pass
+from rest_framework import serializers
+
+from reviews.models import Comment, Review, User
+
+MIN_SCORE = 1
+MAX_SCORE = 10
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """
+    Класс ReviewSerializer.
+
+    Класс ReviewSerializer описывает сериализатор
+    отзывов.
+    """
+
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        default=serializers.CurrentUserDefault(),
+        read_only=True
+    )
+
+    class Meta:
+        fields = ('id', 'author', 'text', 'score', 'pub_date')
+        model = Review
+        read_only_fields = ('author', 'title')
+
+    def validate_score(self, value):
+        if (MIN_SCORE <= value <= MAX_SCORE):
+            return value
+        raise serializers.ValidationError(
+            'Оценка произведения модет быть от 1 до 10'
+        )
+
+    def validate(self, request):
+        if self.context['request'].method != "PATCH":
+            author = request.user
+            title = (
+                request.parser_context['kwargs']['title_id']
+            )
+            if Review.objects.filter(author=author, title=title):
+                raise serializers.ValidationError(
+                    'Можно оставлять только один отзыв')
+        return request
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    Класс CommentSerializer.
+
+    Класс CommentSerializer описывает сериализатор
+    комментариев.
+    """
+
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = ('id', 'author', 'text', 'pub_date')
+        model = Comment
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,1 +1,104 @@
-pass
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+
+from api.permissions import AuthorPermission
+from api.serializers import (
+    CommentSerializer,
+    ReviewSerializer,
+)
+from reviews.models import Review, Title
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """
+    Класс ReviewViewSet.
+
+    Класс ReviewViewSet описывает API-представление отзывов.
+    Предоставляет следующие запросы: GET, POST,
+    PUT, PATCH, DELETE.
+    """
+
+    serializer_class = ReviewSerializer
+    permission_classes = [AuthorPermission]
+    pagination_class = LimitOffsetPagination
+
+    def get_title(self):
+        """
+        Метод get_title.
+
+        Метод get_title возвращает объект
+        отдельного произведения.
+        """
+        return get_object_or_404(
+            Title,
+            id=self.kwargs.get("title_id")
+        )
+
+    def get_queryset(self):
+        """
+        Метод get_queryset.
+
+        Метод get_queryset описывает получение набора
+        объектов/одного объекта модели.
+        """
+        return self.get_title().reviews.all()
+
+    def perform_create(self, serializer):
+        """
+        Метод perform_create.
+
+        Метод perform_create описывает создание новых
+        объектов модели.
+        """
+        serializer.save(
+            author=self.request.user,
+            title=self.get_title()
+        )
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Класс CommentViewSet.
+
+    Класс CommentViewSet описывает API-представление комментариев.
+    Предоставляет следующие запросы: GET, POST,
+    PUT, PATCH, DELETE.
+    """
+
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.AllowAny,)
+    pagination_class = LimitOffsetPagination
+
+    def get_review(self):
+        """
+        Метод get_title.
+
+        Метод get_title возвращает объект
+        отдельного произведения.
+        """
+        return get_object_or_404(
+            Review,
+            id=self.kwargs.get("review_id")
+        )
+
+    def get_queryset(self):
+        """
+        Метод get_queryset.
+
+        Метод get_queryset описывает получение набора
+        объектов/одного объекта модели.
+        """
+        return self.get_review().comments.all()
+
+    def perform_create(self, serializer):
+        """
+        Метод perform_create.
+
+        Метод perform_create описывает создание новых
+        объектов модели.
+        """
+        serializer.save(
+            author=self.request.user,
+            review=self.get_review()
+        )
