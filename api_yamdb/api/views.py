235a9eaa1c174f7bee7ff944a57f690f15c41533
Hengich <daniil.esakov@mail.ru>
2024-07-31 15:55:10 +0300

    Added Nikolay's changes

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import get_object_or_404
 from rest_framework import filters, viewsets
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination
 
-from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
-from reviews.models import Category, Genre, Title
+from .serializers import (CategorySerializer, CommentSerializer,
+                          GenreSerializer, ReviewSerializer, TitleSerializer)
+from reviews.models import Category, Comment, Genre, Review, Title
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -11,7 +13,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     filter_backends = (filters.SearchFilter, )
     search_fields = ('name', )
-    pagination_class = PageNumberPagination
+    pagination_class = LimitOffsetPagination
 
 
 class GenreViewSet(viewsets.ModelViewSet):
@@ -19,7 +21,7 @@ class GenreViewSet(viewsets.ModelViewSet):
     serializer_class = GenreSerializer
     filter_backends = (filters.SearchFilter, )
     search_fields = ('name', )
-    pagination_class = PageNumberPagination
+    pagination_class = LimitOffsetPagination
 
 
 class TitleViewSet(viewsets.ModelViewSet):
@@ -27,3 +29,90 @@ class TitleViewSet(viewsets.ModelViewSet):
     serializer_class = TitleSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ('category', 'genre', 'name', 'year',)
+    pagination_class = LimitOffsetPagination
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """
+    Класс ReviewViewSet.
+    Класс ReviewViewSet описывает API-представление отзывов.
+    Предоставляет следующие запросы: GET, POST,
+    PUT, PATCH, DELETE.
+    """
+
+    serializer_class = ReviewSerializer
+    #permission_classes = [AuthorPermission]
+    pagination_class = LimitOffsetPagination
+
+    def get_title(self):
+        """
+        Метод get_title.
+        Метод get_title возвращает объект
+        отдельного произведения.
+        """
+        return get_object_or_404(
+            Title,
+            id=self.kwargs.get("title_id")
+        )
+
+    def get_queryset(self):
+        """
+        Метод get_queryset.
+        Метод get_queryset описывает получение набора
+        объектов/одного объекта модели.
+        """
+        return self.get_title().reviews.all()
+
+    def perform_create(self, serializer):
+        """
+        Метод perform_create.
+        Метод perform_create описывает создание новых
+        объектов модели.
+        """
+        serializer.save(
+            author=self.request.user,
+            title=self.get_title()
+        )
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Класс CommentViewSet.
+    Класс CommentViewSet описывает API-представление комментариев.
+    Предоставляет следующие запросы: GET, POST,
+    PUT, PATCH, DELETE.
+    """
+
+    serializer_class = CommentSerializer
+    #permission_classes = (permissions.AllowAny,)
+    pagination_class = LimitOffsetPagination
+
+    def get_review(self):
+        """
+        Метод get_title.
+        Метод get_title возвращает объект
+        отдельного произведения.
+        """
+        return get_object_or_404(
+            Review,
+            id=self.kwargs.get("review_id")
+        )
+
+    def get_queryset(self):
+        """
+        Метод get_queryset.
+        Метод get_queryset описывает получение набора
+        объектов/одного объекта модели.
+        """
+        return self.get_review().comments.all()
+
+    def perform_create(self, serializer):
+        """
+        Метод perform_create.
+        Метод perform_create описывает создание новых
+        объектов модели.
+        """
+        serializer.save(
+            author=self.request.user,
+            review=self.get_review()
+        )
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 LENGTH_LIMIT = 50
 
 
@@ -36,3 +38,49 @@ class Title(models.Model):
 
     def __str__(self):
         return self.name[:LENGTH_LIMIT]
+
+
+class Review(models.Model):
+    """
+    Класс Review.
+
+    Класс Review описывает модель отзыва на
+    произведение.
+    """
+
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews')
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews')
+    text = models.TextField('Текст')
+    score = models.IntegerField('Оценка')
+    pub_date = models.DateTimeField(
+        'Дата добавления отзыва', auto_now_add=True, db_index=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_author_title'
+            )
+        ]
+
+    def __str__(self):
+        return self.text[:LENGTH_LIMIT]
+
+
+class Comment(models.Model):
+    """
+    Класс Comment.
+
+    Класс Comment описывает модель комментария к
+    отзывам пользователей.
+    """
+
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        'Дата добавления комментария', auto_now_add=True, db_index=True)
